97723b971aa1a70b75f16bab467a1a1e33f3ca79
TheRitual <dev@theritual.eu>
2019-07-07 01:05:12 +0200

    Tested Hashmap to json operation

--- src/main/java/LaboratoryA.java
+++ src/main/java/LaboratoryA.java
@@ -1,5 +1,7 @@
 public class LaboratoryA {
-    public static void main(String[] args){
+    public static void main(String[] args) throws java.io.IOException {
         System.out.println("Welcome to Laboratory 0");
+
+        FileOperator.testOperation();
     }
 }

c2cce1685fa6def2d1b9c7a5be0470817a1c9f8d
TheRitual <dev@theritual.eu>
2019-07-07 13:10:22 +0200

    Created Class NetFileReader

--- src/main/java/LaboratoryA.java
+++ src/main/java/LaboratoryA.java
@@ -1,7 +1,9 @@
 public class LaboratoryA {
     public static void main(String[] args) throws java.io.IOException {
         System.out.println("Welcome to Laboratory 0");
-
+        //writting lists to json file from hashmaps and arrays
         FileOperator.testOperation();
+
+        System.out.println(NetFileReader.read("http://cognitus.theritual.eu/packages/testfile.txt"));
     }
 }
